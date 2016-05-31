b1b661bbe2a2dcc00187413bd1d3d7960a43ce03
Benjamin Armitano <armitanob@gmail.com>
2016-05-31 23:03:48 +0200

    rails wikinimous in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+10.times do
+articles = Article.create({ title: Faker::Name.title, content: Faker::Lorem.paragraphs(3) })
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
